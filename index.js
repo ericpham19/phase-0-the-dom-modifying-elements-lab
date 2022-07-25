54de18e709f2c6cdfb9d5a25789203418af90ce6
Duy Pham <ericpham19@outlook.com>
2022-07-25 20:52:53 +1000

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+
+
+
+  document.querySelector('main').remove();
+
+  /*it("has a 'newHeader' variable that points to an <h1> node, 'h1#victory'", () => {
+    expect(
+      newHeader.nodeName,
+      "Make sure you create an <h1> with id 'victory'"
+    ).eql("H1");*/
+
+    const newHeader = document.createElement('h1');
+
+    newHeader.id = 'victory';
+    newHeader.innerHTML = "YOUR-NAME is the champion"
+
+    document.body.appendChild(newHeader);
